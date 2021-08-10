9da2f6fdf278d18d9961e221ab69e3d0df57c012
RhuaYuri <61441972+RhuaYuri@users.noreply.github.com>
2021-08-10 13:46:20 -0300

    tela_visuProjeto

--- prototipo/sicov/bin/tela/visu_Projeto.py
+++ prototipo/sicov/bin/tela/visu_Projeto.py
@@ -1,11 +1,14 @@
 import tkinter as t
 from tkinter import ttk
 from util.binario import Dados as dd
+import shelve
+from config.Parametro import BANCO_DADOS
 
 global d_membros
 d_membros = []
 class cadastrarProjeto:
-    def __init__(self):
+    def __init__(self, lista):
+        self.lista = lista
         self.root = t.Tk()
         self.root.geometry("700x500")
         self.w1 = t.LabelFrame(self.root)
@@ -22,25 +25,25 @@ class cadastrarProjeto:
         self.msg2 = t.Label(self.root, text='Nome   ', font='arial 12 bold')
         self.msg2.place(x=12, y=50, relwidth=None, relheight=0.048)
 
-        self.Nome = t.Label(self.root, width=98,  bg='white', relief="groove")
+        self.Nome = t.Label(self.root, text=self.lista.nome, width=98,  bg='white', relief="groove")
         self.Nome.place(x=90, y=55, relwidth=0.85, relheight=0.048)
 
         self.msg3 = t.Label(self.root, text='Data     ', font='arial 12 bold')
         self.msg3.place(x=12, y=85, relwidth=None, relheight=0.048)
 
-        self.Data = t.Label(self.root, width=45, bg='white', relief="groove")
+        self.Data = t.Label(self.root, text=lista.data, width=45, bg='white', relief="groove")
         self.Data.place(x=90, y=84, relwidth=None, relheight=0.048)
 
         self.msg4 = t.Label(self.root, text='Descrição  ', font='arial 12 bold')
         self.msg4.place(x=12, y=120)
 
-        self.Descricao = t.Label(self.root, width=83, height = 5, bg='white', relief="groove")
+        self.Descricao = t.Label(self.root, text=self.lista.descricao, width=83, height = 5, bg='white', relief="groove")
         self.Descricao.place(x=15, y=150, relwidth=0.96, relheight=0.16)
 
         self.msg5 = t.Label(self.root, text='EQUIPE', font='arial 16 bold')
         self.msg5.place(x=12, y=270, relwidth=None, relheight=0.048)
 
-        self.tree = ttk.Treeview(self.root, selectmode="browse", column=("coluna1", "coluna2", "coluna3", "coluna4", "coluna5"), show="headings")
+        self.tree = ttk.Treeview(self.root, selectmode="browse", column=("coluna1", "coluna2", "coluna3", "coluna4"), show="headings")
 
         
         self.vsb = ttk.Scrollbar(self.root, orient="vertical", command=self.tree.yview)
@@ -59,9 +62,6 @@ class cadastrarProjeto:
 
         self.tree.column("coluna4", width=100, minwidth=50)
         self.tree.heading('#4', text='Coordenador')
-
-        self.tree.column("coluna5", width=100, minwidth=50)
-        self.tree.heading('#5', text='Ações')
         
         self.visu()
         
@@ -77,15 +77,21 @@ class cadastrarProjeto:
 
     def mudarTela(self):
         self.root.destroy()
-        from tela.transicao import Transicao as T
-        T(n=1)
 
     def cancelar(self, tela):
         tela.destroy()
     
 
     def visu(self):
-        self.dados = dd()
+        for c in self.lista.pesquisadores:
+            self.tree.insert("", 'end',values=[c.matricula, c.nome, c.tipoMembro, c.coordenador], tag='1')
+
+
+
+
+
+
+        '''self.dados = dd()
         self.informacoes = self.dados.lerBin()
         self.cont = 1
         try:
@@ -98,5 +104,4 @@ class cadastrarProjeto:
                         break
                 self.cont += 1
         except:
-            self.tree.insert("", 'end',values=[], tag='1')
-cadastrarProjeto()
+            self.tree.insert("", 'end',values=[], tag='1')'''
